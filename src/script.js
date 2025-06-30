0a289749ccb081c2ebeaa7c662fe0a20e8838c10
VoGiaHuy <huyvog6226@gmail.com>
2025-06-30 14:28:13 +0700

    done module discount

--- src/script.js
+++ src/script.js
@@ -1,30 +1,58 @@
 /*NowNews */
 const slider = document.getElementById('sliderWrapper');
-        const prevBtn = document.getElementById('prevBtn');
-        const nextBtn = document.getElementById('nextBtn');
+    const prevBtn = document.getElementById('prevBtn');
+    const nextBtn = document.getElementById('nextBtn');
 
-        const scrollAmount = 1210;
+    const scrollAmount = 1210;
 
-        prevBtn.addEventListener('click', () => {
-            slider.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
-        });
+    prevBtn.addEventListener('click', () => {
+        slider.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+    });
 
-        nextBtn.addEventListener('click', () => {
-            slider.scrollBy({ left: scrollAmount, behavior: 'smooth' });
-        });
+    nextBtn.addEventListener('click', () => {
+        slider.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    });
 
 
 /*Phim đang chiếu**/ 
 const slide = document.getElementById('Wrapper');
-        const prevButn = document.getElementById('prevButn');
-        const nextButn = document.getElementById('nextButn');
+    const prevButn = document.getElementById('prevButn');
+    const nextButn = document.getElementById('nextButn');
 
-        const Amount = 1365;
+    const Amount = 1365;
 
-        prevButn.addEventListener('click', () => {
-            slide.scrollBy({ left: -Amount, behavior: 'smooth' });
-        });
+    prevButn.addEventListener('click', () => {
+        slide.scrollBy({ left: -Amount, behavior: 'smooth' });
+    });
 
-        nextButn.addEventListener('click', () => {
-            slide.scrollBy({ left: Amount, behavior: 'smooth' });
-        });
+    nextButn.addEventListener('click', () => {
+        slide.scrollBy({ left: Amount, behavior: 'smooth' });
+    });
+
+
+/*Phim sắp chiếu*/
+const wrapper = document.getElementById('futureWrapper');
+    const prevBtn2 = document.getElementById('prevBtn2');
+    const nextBtn2 = document.getElementById('nextBtn2');
+
+    const scrollAmount2 = 360;
+
+    prevBtn2.addEventListener('click', () => {
+        wrapper.scrollBy({ left: -scrollAmount2, behavior: 'smooth' });
+    });
+    nextBtn2.addEventListener('click', () => {
+        wrapper.scrollBy({ left: scrollAmount2, behavior: 'smooth' });
+    });
+
+/*Khuyến mãi*/
+const discountWrapper = document.getElementById('discountsWrapper');
+    const prevDiscountBtn = document.getElementById('prevDiscountsBtn');
+    const nextDiscountBtn = document.getElementById('nextDiscountsBtn');
+
+    const discountScrollAmount = 1360;
+    prevDiscountBtn.addEventListener('click', () => {
+        discountWrapper.scrollBy({ left: -discountScrollAmount, behavior: 'smooth' });
+    });
+    nextDiscountBtn.addEventListener('click', () => {
+        discountWrapper.scrollBy({ left: discountScrollAmount, behavior: 'smooth' });
+    });
