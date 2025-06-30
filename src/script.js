f0f533eaad0492bc0eb99c15d496de1d1a7f3e02
VoGiaHuy <huyvog6226@gmail.com>
2025-06-30 09:43:58 +0700

    done module showing

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-
+/*NowNews */
 const slider = document.getElementById('sliderWrapper');
         const prevBtn = document.getElementById('prevBtn');
         const nextBtn = document.getElementById('nextBtn');
@@ -11,4 +11,20 @@ const slider = document.getElementById('sliderWrapper');
 
         nextBtn.addEventListener('click', () => {
             slider.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+        });
+
+
+/*Phim đang chiếu**/ 
+const slide = document.getElementById('Wrapper');
+        const prevButn = document.getElementById('prevButn');
+        const nextButn = document.getElementById('nextButn');
+
+        const Amount = 1365;
+
+        prevButn.addEventListener('click', () => {
+            slide.scrollBy({ left: -Amount, behavior: 'smooth' });
+        });
+
+        nextButn.addEventListener('click', () => {
+            slide.scrollBy({ left: Amount, behavior: 'smooth' });
         });
